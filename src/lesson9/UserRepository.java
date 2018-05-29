ec62b5bc86ed473f20c175231ae1f37b7eb5826d
MashaJM <masha.chip@gagadget.com>
2018-05-30 01:17:42 +0300

    Added getUserByName, getUserById, getUserBySessionId methods

--- src/lesson9/UserRepository.java
+++ src/lesson9/UserRepository.java
@@ -56,24 +56,89 @@ public class UserRepository {
         }
     }
 
- /*  public static void main(String[] args) {
+   /* public User[] getUserByName (String name) {
+        User[] usersWithName;
+        int counter = 0;
+        for (User elem : users) {
+            if (elem != null) {
+                if (elem.getName().equals(name)) {
+                    counter++;
+                }
+            }
+        }
+        usersWithName = new User[counter];
+        int counter2 = 0;
+        for (int i = 0; i <= counter; i++) {
+            if (users[i].getName().equals(name)) {
+                usersWithName[counter2] = users[i];
+                counter2++;
+                System.out.println(users[i].getId() + " added");
+            }
+
+        }
+        return usersWithName;
+    } */
+
+    public User getUserByName(String name) {
+        User usersWithName;
+
+        for (User elem : users) {
+            if (elem != null) {
+                if (elem.getName().equals(name)) {
+                    return elem;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public User getUserByID(long id) {
+        User userWithId;
+        for (User elem : users) {
+            if (elem != null) {
+                if (elem.getId() == id) {
+                    return elem;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public User getUserBySessionID(String sessionId) {
+        User userWithId;
+        for (User elem : users) {
+            if (elem != null) {
+                if (elem.getSessionId() == sessionId) {
+                    return elem;
+                }
+            }
+        }
+        return null;
+    }
+
+
+   /* public static void main(String[] args) {
 
         User vasia = new User(1, "Vasya", "FirstSession");
         User vova = new User(2, "Vladimir", "First Session");
+        //User anotherVasia = new User(3, "Vasya", "First Session");
 
-        User[] users = new User[2];
-       // users[0]= vasia;
+        User[] users = new User[3];
+        users[0] = vasia;
         users[1] = vova;
+        //  users[2] = anotherVasia;
 
         UserRepository userRepository = new UserRepository(users);
-        System.out.println(userRepository.getUserNameById(1));
-        System.out.println(userRepository.getUserNameById(3));
-        System.out.println(userRepository.getUserNameById(2));
+        // System.out.println(userRepository.getUserNameById(1));
+        // System.out.println(userRepository.getUserNameById(3));
+        //  System.out.println(userRepository.getUserNameById(2));
+        //   System.out.println(Arrays.toString(userRepository.getUserNames()));
 
+        System.out.println((userRepository.getUserByName("Vov")));
 
-        System.out.println(Arrays.toString(userRepository.getUserNames()));
-
-    }*/
+    } */
 
 
 }
