9191e458530e88bdbfb7e3ac7667bb70a3a2ec53
MashaJM <masha.chip@gagadget.com>
2018-06-26 02:34:44 +0300

    11th lesson hw + BookingCom method

--- src/lesson11/lesson11hw/BookingComAPI.java
+++ src/lesson11/lesson11hw/BookingComAPI.java
@@ -1,4 +1,41 @@
 package lesson11.lesson11hw;
 
-public class BookingComAPI {
-}
+public class BookingComAPI implements API {
+    Room[] rooms;
+
+    public BookingComAPI(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        int counter = 0;
+        for (Room room : rooms) {
+            if ((((room.getPrice() > (price - 100)) &&
+                    (room.getPrice() <= (price + 100)) &&
+                    (room.getPersons() == persons) && (room.getCityName() == city)
+                    && (room.getHotelName() == hotel))))
+                counter++;
+        }
+        Room[] availableRooms = new Room[counter];
+        int k = 0;
+        for (int i = 0; i < counter; i++) {
+            if ((((rooms[i].getPrice() > (price - 100)) &&
+                    (rooms[i].getPrice() <= (price + 100)) &&
+                    (rooms[i].getPersons() == persons) && (rooms[i].getCityName() == city)
+                    && (rooms[i].getHotelName() == hotel)))) {
+                System.out.println("room in " + hotel + " is available from" + rooms[i].getDateAvailableFrom());
+                availableRooms[k] = rooms[i];
+                k++;
+            }
+        }
+            return availableRooms;
+        }
+
+
+        @Override
+        public Room[] getAll () {
+            return rooms;
+        }
+    }
+
